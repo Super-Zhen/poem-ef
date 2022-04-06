d6b7f7791dc03fbc47521de6ef7cab63734879b8
zhenchaoqun <zhenchaoqun@2858.com>
2022-04-06 22:21:40 +0800

    添加号码检测

--- pages/me/register.vue
+++ pages/me/register.vue
@@ -7,7 +7,7 @@
 			<image :src="imgUrl" mode="widthFix" class="width80" @tap="getCodeImg"></image>
 			<input type="text" v-model="codes" placeholder="验证码">
 		</view>
-		<button class="loginBtn mgt20" @tap="toNext">下一步</button>
+		<button class="loginBtn mgt20"  @tap="toNext">下一步</button>
 	</view>
 </template>
 
@@ -29,6 +29,17 @@
 		onLoad() {
 			this.getCodeImg()
 		},
+		watch:{
+			phone:function(val,oldval){
+				if(oldval.length==11){
+					if(val!=oldval){
+						this.isDisabled = false
+					}
+				}else{
+					// this.isDisabled = true
+				}
+			}
+		},
 		methods: {
 			getCodeImg() {
 				let data = randomString()
@@ -36,7 +47,7 @@
 				this.$api.user.codeImg({
 					data
 				}).then(res => {
-					console.log(res)
+					this.codes = ''
 					this.imgUrl = res.data.imgUrl
 					this.code = res.data.text
 					this.randomData = res.data.codeData
@@ -56,16 +67,28 @@
 					return
 				}
 				if (this.codeData != this.randomData || this.code != this.codes.toLowerCase()) {
+					this.getCodeImg()
 					dialog.toast({
-						msg: '请刷新验证码之后重试'
+						msg: '验证码错误请重试'
 					})
 					return
 				}
-				debugger
 				this.$api.user.userIsExist({
 					phone: this.phone
 				}).then(res => {
 					console.log(res)
+					if(res.data){
+						this.$yrouter.push({
+							path:''
+						})
+					}else{
+						// this.isDisabled = true
+						this.codes = ''
+						this.getCodeImg()
+						dialog.toast({
+							msg:res.message
+						})
+					}
 				})
 			}
 		}
