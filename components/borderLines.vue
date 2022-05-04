f3fad29b3f2b10b0c120f238ccccb5512709ecd2
zhenchaoqun <zhenchaoqun@2858.com>
2022-05-04 23:03:24 +0800

    添加设置页面

--- components/borderLines.vue
+++ components/borderLines.vue
@@ -1,15 +1,177 @@
 <template>
-		<view class="lineBorder bgcf3f3f3">
-		</view>
+	<view class="">
+		<template v-for="(items, indexs) in list">
+			<view class="lineBorder bgcf3f3f3"></view>
+			 <view :key="indexs" class="items">
+				<view class="title" v-if='items.title'>{{items.title}}</view>
+				<template v-for="(item,index) in items.listItem" >
+					<view class="df aic jcsb item borderB bgCfff" :key="index">
+						<text class="fontS14">{{item.title}}</text>
+						<view class="df aic" v-if="item.icon" @tap='toPage(item)'>
+							<text v-if="item.label" class="fontS14">{{item.label}}</text>
+							<uni-icons :type="item.icon" :color="item.color?item.color:'#666666'" size="20"  />
+						</view>
+						<view v-else-if="item.switch">
+							<switch v-if='item.flag=="collect"' :checked='collectChecked' @change='changeCollect' style="transform:scale(0.8)"/>
+							<switch v-if='item.flag=="type"' :checked='typeChecked' @change='changeType' style="transform:scale(0.8)"/>
+						</view>
+					</view>
+				</template>
+			 </view>
+		</template>
+	</view>
 </template>
 
 <script>
+	import uniIcons from '@/components/uni-icons/uni-icons.vue'
 	export default {
 		name:"borderLines",
 		data() {
 			return {
-				
+				collectChecked: true,
+				typeChecked:true,
+				list:[
+					{
+						title:'账号和隐私',
+						listItem:[
+							{
+								title:'个人信息',
+								icon:'right',
+								url:''
+							},
+							{
+								title:'账号设置',
+								icon:'right',
+								url:''
+							},
+							{
+								title:'隐私设置',
+								icon:'right',
+								url:''
+							},
+							
+						]
+					},
+					{
+						title:'工具',
+						listItem:[
+							{
+								title:'桌面插件',
+								icon:'right',
+								url:''
+							},
+							{
+								title:'锁屏模式',
+								icon:'right',
+								url:''
+							},
+							{
+								title:'简繁切换',
+								icon:'right',
+								url:'',
+								label:'简体'
+							},
+							
+						]
+					},
+					{
+						title:'摘录',
+						listItem:[
+							{
+								title:'摘录范围',
+								icon:'right',
+								url:''
+							},
+							{
+								title:'摘录字体',
+								icon:'right',
+								url:'',
+								label:'古体'
+							},
+							{
+								title:'右滑收藏',
+								switch:'1',
+								flag:'collect',
+								url:'',
+								label:'简体'
+							},
+							
+						]
+					},
+					{
+						title:'诗词',
+						listItem:[
+							{
+								title:'注释位置',
+								icon:'right',
+								url:'',
+								label:'上方'
+							},
+							{
+								title:'阅读背景',
+								icon:'right',
+								url:'',
+								label:'鸟鸣涧'
+							},
+							{
+								title:'诗词字体',
+								icon:'right',
+								url:'',
+								label:'默认'
+							},
+							
+						]
+					},
+					{
+						title:'文库',
+						listItem:[
+							{
+								title:'分类样式',
+								icon:'right',
+								url:'',
+								label:'图文',
+							}
+						]
+					},
+					{
+						title:'推送',
+						listItem:[
+							{
+								title:'日赏推送',
+								switch:'1',
+								url:'',
+								flag:'type'
+							}
+						]
+					},
+					{
+						title:'系统',
+						listItem:[
+							{
+								title:'清除缓存',
+								icon:'right',
+								url:'',
+								label:'0.0M'
+							}
+						]
+					}
+				]
 			};
+		},
+		components:{
+			uniIcons
+		},
+		methods:{
+			toPage(url){
+				console.log(url)
+			},
+			changeCollect(e){
+				this.collectChecked = e.detail.value
+				// console.log(e)
+			},
+			changeType(e){
+				this.typeChecked = e.detail.value
+			}
 		}
 	}
 </script>
@@ -20,5 +182,18 @@
 	border-right: 0;
 	border-left: 0;
 	height: 40rpx;
+}
+.title{
+	color: #999999;
+	font-size: 26rpx;
+	line-height: 60rpx;
+	padding-left: 40rpx;
+	background-color: #FFFFFF;
+}
+.items{
+	color: #666666;
+}
+.item{
+	padding: 20rpx 40rpx;
 }
 </style>
